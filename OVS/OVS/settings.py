7590e602382b3f026fa8d889b4493343f0a05071
MShrinath <112850938+MShrinath@users.noreply.github.com>
2024-03-07 10:11:46 +0530

    usernametopleft

--- OVS/OVS/settings.py
+++ OVS/OVS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n+^n)+xw^rj+c!+mnz!h_gtggky+e=41)4_c4ntpdm+pqn@-ph
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_matplotlib',
     'adminapp'
 ]
 
--- OVS/adminapp/admin.py
+++ OVS/adminapp/admin.py
@@ -1,6 +1,12 @@
-# adminapp/admin.py
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 from .models import Voter
 
-admin.site.register(Voter)
+class VoterAdmin(UserAdmin):
+    list_display = ('id', 'username', 'vote' ,'is_staff', 'is_voted')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_staff', 'is_voted', 'gender')
+
+
+admin.site.register(Voter,VoterAdmin)
+# admin.site.register(Voter)
