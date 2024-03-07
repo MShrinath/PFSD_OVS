d5eab9ab2329c40c396ac9b43450949a2bfc8cf9
MShrinath <112850938+MShrinath@users.noreply.github.com>
2024-03-07 11:46:02 +0530

    ok nice test

--- OVS/OVS/settings.py
+++ OVS/OVS/settings.py
@@ -31,13 +31,18 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'matplotlib',
+    'django_matplotlib',
+
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_matplotlib',
+
+
     'adminapp'
 ]
 
--- OVS/adminapp/models.py
+++ OVS/adminapp/models.py
@@ -3,28 +3,27 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class Voter(AbstractUser):
-    id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=30, blank=False)
-    last_name = models.CharField(max_length=30, blank=False)
     date_of_birth = models.DateField(blank=True, null=True)
     aadhar_number = models.CharField(max_length=12, blank=True, null=True, unique=True)
     voter_id = models.CharField(max_length=20, blank=True, null=True, unique=True)
-    
+
     GENDER_CHOICES = [
         ('M', 'Male'),
         ('F', 'Female'),
         ('O', 'Other'),
     ]
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True, null=True)
-    
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=150, unique=True, blank=False)
-    password = models.CharField(max_length=128, blank=False)
-    vote = models.IntegerField (default=-1,blank=False)
+
+    VOTE_CHOICES = [
+        (-1, 'Not Voted'),
+        (0, 'Candidate 1'),
+        (1, 'Candidate 2'),
+    ]
+    vote = models.IntegerField(default=-1, choices=VOTE_CHOICES)
     is_voted = models.BooleanField(default=False)
     
     def __str__(self):
-        return self.username
-    
+        return self.get_full_name()
+
     class Meta:
         db_table = "voter"
