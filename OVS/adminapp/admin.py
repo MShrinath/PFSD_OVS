c3e60afbf8c1d931a405001aa616218be1a25ed2
MShrinath <112850938+MShrinath@users.noreply.github.com>
2024-03-09 04:23:39 +0530

    fixed admin seeng vote, not graph

--- OVS/adminapp/admin.py
+++ OVS/adminapp/admin.py
@@ -4,8 +4,15 @@ from .models import Voter
 
 class VoterAdmin(UserAdmin):
     list_display = ('id', 'username', 'vote' , 'is_voted')
-    search_fields = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('username', 'email', 'first_name', 'last_name','vote','is_voted' )
     list_filter = ('vote', 'gender')
+    
+    fieldsets = (
+            (None, {'fields': ('username', 'password')}),
+            ('Personal Info', {'fields': ('first_name', 'last_name', 'email')}),
+            ('Verification Info', {'fields': ('date_of_birth', 'aadhar_number', 'voter_id')}),
+            ('Voting Info', {'fields': ('vote', 'is_voted')}),
+        )
 
 admin.site.register(Voter,VoterAdmin)
 
